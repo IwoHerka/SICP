b5ef6830b7a9f8a43202f7e8f8fdcbae15aa5386
Siegmeyer <herkaiwo@gmail.com>
2017-06-16 18:55:05 +0200

    1.7

--- exercises.scm
+++ exercises.scm
@@ -8,3 +8,21 @@
   (cond ((and (< x y) (< x z)) (sum-of-squares y z))
         ((and (< y x) (< y z)) (sum-of-squares x z))
         (else (sum-of-squares x y))))
+
+
+;; Exercise 1.7 ----------------------------------------------------------------
+
+(define (sqrt x precision)
+  (define (enough? new old)
+    (< (abs (- new old)) precision))
+  (define (avg x y)
+    (/ (+ x y) 2))
+  (define (improve guess)
+    (avg guess (/ x guess)))
+  (define (sqrt-iter guess prev-guess)
+    (if (enough? guess prev-guess)
+         guess
+         (sqrt-iter (improve guess) guess)))
+  (sqrt-iter 1.0 0.0))
+
+
