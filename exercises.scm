52a859cb4381330e505b4a849e57f10abec0d901
Siegmeyer <herkaiwo@gmail.com>
2017-06-17 12:56:19 +0200

    1.8

--- exercises.scm
+++ exercises.scm
@@ -25,4 +25,16 @@
          (sqrt-iter (improve guess) guess)))
   (sqrt-iter 1.0 0.0))
 
+;; Exercise 1.8 ----------------------------------------------------------------
 
+(define (sqcb x precision)
+  (define (enough? new old)
+    (< (abs (- new old)) precision))
+  (define (improve guess)
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+  (define (sqcb-iter guess prev-guess)
+    (if (enough? guess prev-guess)
+        guess
+        (sqcb-iter (improve guess) guess)))
+  (sqcb-iter 1.0 0.0))
+    
