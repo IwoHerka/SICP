03049566be7948d24fe3e018bb4d03c606773c38
Siegmeyer <herkaiwo@gmail.com>
2017-07-13 22:55:44 +0200

    1.22 #1

--- exercises.scm
+++ exercises.scm
@@ -109,3 +109,41 @@
                           p
                           q
                           (- count 1)))))
+
+;; Exercise 1.22 ---------------------------------------------------------------
+
+(define (smallest-divisor n)
+    (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+    (= (remainder b a) 0))
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+    (define (start-prime-test n start-time)
+        (define (report-prime elapsed-time)
+            (display " *** ")
+            (display elapsed-time)
+            elapsed-time)
+        (if (prime? n)
+            (report-prime (- (runtime) start-time))
+            0))
+    (newline)
+    (display n)
+    (start-prime-test n (runtime)))
+
+(define (test-primes-in-range start end elapsed)
+    (if (>= end start)
+        elapsed
+        (test-primes-in-range (+ start 1) end (+ elapsed (timed-prime-test start)))))
+    
+    
+
+
